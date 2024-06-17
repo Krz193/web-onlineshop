32dad7aaf9b4ea069ff11aa4043ccde96148a309
nekoLin <krusiawanbudi1@gmail.com>
2024-06-17 14:25:06 +0800

    complete crud product

--- App/Product.php
+++ App/Product.php
@@ -24,9 +24,68 @@ class Product extends Database
         return mysqli_fetch_assoc($this->runQuery($sql));
     }
 
+    public function addProduct(array $data) 
+    {
+        $cat_id         = $data['category_id'];
+        $product_name   = $data['product_name'];
+        $product_price  = $data['product_price'];
+        $product_desc   = $data['product_description'];
+        $product_status = $data['product_status'];
+        $date_created   = $this->date->format('Y-m-d H:i:s');
+
+        $target_dir     = 'uploads/';
+        $file           = pathinfo($_FILES['product_image']['name'], PATHINFO_ALL);
+        $new_filename   = $this->date->format('YmdHi') . '_' . $file['basename'];
+        $uploaded_file  = $target_dir . $new_filename;
+
+        $sql            = "INSERT INTO tb_product(category_id, product_name, product_price, product_description, product_image, product_status, date_created)
+                        VALUES('$cat_id', '$product_name', '$product_price', '$product_desc', '$new_filename', '$product_status', '$date_created')";
+
+        if(move_uploaded_file($_FILES['product_image']['tmp_name'], $uploaded_file)) {
+            return $this->runQuery($sql);
+        } else echo "<script>alert('failed to upload file')</script>";
+    }
+
+    public function updateProduct(array $data)
+    {
+        $product_id     = $data['product_id'];
+        $cat_id         = $data['category_id'];
+        $product_name   = $data['product_name'];
+        $product_price  = $data['product_price'];
+        $product_desc   = $data['product_description'];
+        $product_status = $data['product_status'];
+        $date_created   = $this->date->format('Y-m-d H:i:s');
+
+        $new_filename   = $data['old_img'];
+        $target_dir     = 'uploads/';
+
+        if($data['old_img'] != $_FILES['product_image']['name'] && $_FILES['product_image']['name'] != '') {
+            $file           = pathinfo($_FILES['product_image']['name'], PATHINFO_ALL);
+            if(file_exists($target_dir.$new_filename)) unlink($target_dir.$new_filename);
+            $new_filename   = $this->date->format('YmdHi') . '_' . $file['basename'];
+            if(!move_uploaded_file($_FILES['product_image']['tmp_name'], $target_dir.$new_filename)) echo "<script>alert('failed to upload file')</script>";
+        }
+
+        $sql = "UPDATE tb_product SET 
+                category_id = '$cat_id',
+                product_name = '$product_name',
+                product_price = '$product_price',
+                product_description = '$product_desc',
+                product_image = '$new_filename',
+                product_status = '$product_status',
+                date_created = '$date_created' WHERE product_id = $product_id";
+
+        return $this->runQuery($sql);
+    }
+
     public function deleteProduct($id) 
     {
-        return $this->runQuery("DELETE FROM tb_product WHERE user_id = $id");
+        $product = $this->getProduct($id);
+        if($this->runQuery("DELETE FROM tb_product WHERE product_id = $id")) {
+            if(file_exists('uploads/'.$product['product_image'])) 
+                if(!unlink('uploads/'.$product['product_image'])) echo "<script>alert('gagal menghapus file gambar')</script>";
+            return true;
+        } else return false;
     }
 
     // CRUD table review
