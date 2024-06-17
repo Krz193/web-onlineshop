e9a42e5c95c29df439dc2f3434b5683c9e5b6564
nekoLin <krusiawanbudi1@gmail.com>
2024-06-17 14:27:35 +0800

    progress role separation

--- nav.php
+++ nav.php
@@ -1,5 +1,6 @@
 <nav class="w-4/12 p-3 mx-auto flex justify-evenly">
     <a href="view_product.php" class="capitalize font-bold text-lg text-gray-700 hover:text-gray-950">home</a>
     <a href="view_user.php" class="capitalize font-bold text-lg text-gray-700 hover:text-gray-950">user</a>
+    <a href="" class="capitalize font-bold text-lg text-gray-700 hover:text-gray-950">history</a>
     <a href="" class="capitalize font-bold text-lg text-gray-700 hover:text-gray-950">purchase</a>
 </nav>
